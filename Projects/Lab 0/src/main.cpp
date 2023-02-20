a34adb25e253f9cad1fba1d78652c526bc195a05
VincentMagat <junevincent.magat@gmail.com>
2023-02-19 19:59:05 -0800

    added photoresister/ finished lab 0

--- Projects/Lab 0/src/main.cpp
+++ Projects/Lab 0/src/main.cpp
@@ -2,29 +2,44 @@
 #include <Servo.h>
 Servo myservo; // creating object for servo
 
+int lightIn;
+int lightMax;
+int lightCurrent;
+
+int servo_out; //currebnt value output to servo
+int convert = 22; 
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(9102);
   pinMode(13, OUTPUT);
   myservo.attach(2);
 
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
   int i = 0;
 
   while (i <=10){
+    Serial.println(i);
     digitalWrite(13, HIGH);
     delay(500);
     digitalWrite(13, LOW);
     delay(500);
     i++;
   }
-  delay(4000);
-  myservo.write(50);
-  delay(4000);
 
+  lightIn = analogRead(15);
+
+  delay(4000);
+  Serial.print("Callibration complete \n");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  lightCurrent = analogRead(15); // takes in inout value from photosensor
+  if (lightCurrent > lightIn) {
+    servo_out = lightCurrent/22;
+    myservo.write(servo_out);
+  }
 
 }
